2cfc49ba31a34a54363f83f793d0213fb792c847
ArsellyaDwi <arsellyad@gmail.com>
2026-05-09 16:07:30 +0700

    add ContactController API for customer contact form

---
+++ app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\ContactMessage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ContactController extends Controller
+{
+    public function send(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string|min:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $message = ContactMessage::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'subject' => $request->subject,
+            'message' => $request->message,
+            'status' => 'unread',
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Your message has been sent successfully! We will reply within 24 hours.',
+            'data' => [
+                'id' => $message->id,
+                'created_at' => $message->created_at
+            ]
+        ], 201);
+    }
+}
