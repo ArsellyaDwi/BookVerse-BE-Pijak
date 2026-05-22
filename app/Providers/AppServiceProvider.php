6be669f74666d1350883b2585f324254cfe468b0
ArsellyaDwi <arsellyad@gmail.com>
2026-05-22 19:10:46 +0700

    increate http timeout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Http::globalOptions([
-            'timeout' => 60,
+            'timeout' => 60 * 10,
         ]);
     }
 }
